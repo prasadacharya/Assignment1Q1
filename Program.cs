948042cfbc42ae20092654fce093ff5dac687902
prasadacharya <prasada@mail.usf.edu>
2020-01-23 00:22:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AssignmentQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input number");
+
+            string N = Console.ReadLine();
+
+            int n = Convert.ToInt32(N);
+
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = n-i; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
